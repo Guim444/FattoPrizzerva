bc39f8bc23dc49fc5a64628b8948a5f7c3bc30b1
Jan España <jan.espana.7e7@itb.cat>
2025-12-02 13:03:03 +0100

    Chess board auto-generation made. Pawn movement made too. 2/12/2025

--- Assets/Scripts/PacifistGamesScripts/PawnGameScript/BoardManager.cs
+++ Assets/Scripts/PacifistGamesScripts/PawnGameScript/BoardManager.cs
@@ -5,10 +5,13 @@ public class BoardManager : MonoBehaviour
 {
     public static BoardManager instance;
     public GameObject pawnPrefab;
+    public GameObject whiteSquarePrefab, blackSquarePrefab, boardPrefab;
+    public List<GameObject> rowCount = new List<GameObject>();
+    public float height, width;
 
     public Dictionary<string, ChessSquareScript> squares = new Dictionary<string, ChessSquareScript>();
 
-    void Awake()
+    public void Awake()
     {
         if (instance == null)
         {
@@ -19,8 +22,54 @@ public class BoardManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-    }
 
+        GenerateBoard();
+    }
+    public void GenerateBoard()
+    {
+        List<GameObject> fullBoard = new List<GameObject>();
+
+        GameObject board = Instantiate(boardPrefab, transform.position, Quaternion.identity);
+        board.transform.parent = transform;
+        board.transform.localScale = new Vector3(height + 0.5f, 1, width + 0.5f);
+
+        for (int i = 0; i < height; i++)
+        {
+            GameObject rowParent = new GameObject("Row" + (i + 1));
+            rowParent.transform.parent = board.transform;
+
+            float rowX = height / 2f - 0.5f - i;
+            Vector3 rowStartPos = new Vector3(rowX, 0.15f, -(width / 2f - 0.5f));
+            GameObject previousSquare = null;
+
+            for (int j = 0; j < width; j++)
+            {
+                GameObject prefab = ((i + j) % 2 == 0) ? whiteSquarePrefab : blackSquarePrefab;
+
+                GameObject square = Instantiate(prefab);
+                square.transform.parent = rowParent.transform;
+
+                if (previousSquare == null)
+                {
+                    square.transform.position = rowStartPos;
+                }
+                else
+                {
+                    Vector3 prevPos = previousSquare.transform.position;
+                    square.transform.position = new Vector3(prevPos.x, prevPos.y, prevPos.z + 1f);
+                }
+
+                previousSquare = square;
+
+                ChessSquareScript css = square.GetComponent<ChessSquareScript>();
+                if (css != null)
+                {
+                    css.SquareColumn = (char)('A' + j);
+                }
+                css.CreateSquare();
+            }
+        }
+    }
     public void RegisterSquare(ChessSquareScript square)
     {
         if (!squares.ContainsKey(square.name))
@@ -29,6 +78,11 @@ public class BoardManager : MonoBehaviour
         }
         else
             Debug.LogWarning("Duplicate square: " + square.name);
+
+        if (squares.Count == height * width)
+        {
+            SpawnPawns((int)width);
+        }
     }
 
     public ChessSquareScript GetSquare(string name)
@@ -42,7 +96,7 @@ public class BoardManager : MonoBehaviour
         int row = 1;
         int spawned = 0;
 
-        for (char col = 'A'; col <= 'H' && spawned < quantity; col++)
+        for (char col = 'A'; spawned < quantity; col++)
         {
             string squareName = col.ToString() + row.ToString();
             ChessSquareScript square = GetSquare(squareName);
--- Assets/Scripts/PacifistGamesScripts/PawnGameScript/PawnBehavior.cs
+++ Assets/Scripts/PacifistGamesScripts/PawnGameScript/PawnBehavior.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.IO;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 using static UnityEngine.Rendering.DebugUI.Table;
+using UnityEngine.UI;
 
 public class PawnBehavior : MonoBehaviour
 {
     public ChessSquareScript currentSquare;
+    bool isMoving = false; //We avoid strange movements
+    List<ChessSquareScript> possiblePaths = new List<ChessSquareScript>();
+    public int[] possibleMovements;
+
     void Awake()
     {
         if (currentSquare != null)
@@ -14,10 +21,56 @@ public class PawnBehavior : MonoBehaviour
             transform.position = currentSquare.pawnPosition;
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
+        ClickManager.instance.selectedPawn = this;
+        TrackAllPaths();
+    }
+    public ChessSquareScript FindNextSquare(int moveAmount)
+    {
+        int nextRow = currentSquare.SquareRow;
+        nextRow += moveAmount;
+        ChessSquareScript nextSquare = BoardManager.instance.GetSquare(currentSquare.SquareColumn.ToString() + nextRow.ToString());
+        if (nextSquare != null)
+        {
+            return nextSquare;
+        }
+        return currentSquare;
+    }
+    public void TrackAllPaths()
+    {
+        foreach (int move in possibleMovements)
+        {
+            ChessSquareScript square = FindNextSquare(move);
 
+            square.ToggleGlow(true);
+            possiblePaths.Add(square);
+        }
+    }
+    public void Deselect()
+    {
+        ClickManager.instance.selectedPawn = null;
+        foreach (var square in possiblePaths)
+            square.ToggleGlow(false);
+
+        possiblePaths.Clear();
+    }
+    public IEnumerator MoveForward(ChessSquareScript nextSquare)
+    {
+        isMoving = true;
+        float elapsed = 0;
+
+        while (elapsed < 1)
+        {
+            elapsed += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(transform.position, nextSquare.pawnPosition, elapsed);
+            yield return null;
+        }
+
+        transform.position = nextSquare.pawnPosition;
+        currentSquare = nextSquare;
+
+        isMoving = false;
     }
 }
